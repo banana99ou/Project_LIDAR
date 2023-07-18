53635bcc49100317959073c0a57c451180573928
banana <banana99ou@gmail.com>
2023-07-18 20:24:17 +0900

    idk

--- lidar/stoplidar.py
+++ lidar/stoplidar.py
@@ -3,7 +3,7 @@ from adafruit_rplidar import RPLidar
 PORT_NAME = '/dev/ttyUSB0'
 lidar = RPLidar(None, PORT_NAME, timeout=3)
 for i in range(1000):
-    lidar.stop()
+    # lidar.stop()
     lidar.stop_motor()
     lidar.disconnect()
     time.sleep(1)
--- servo/StepperMotorTest/StepperMotorTest.ino
+++ servo/StepperMotorTest/StepperMotorTest.ino
@@ -18,21 +18,23 @@ void loop() {
  	Serial.println(F("Running clockwise"));
  	digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction
  	// Makes 200 pulses for making one full cycle rotation
- 	for (int i = 0; i < stepsPerRev; i++) {
+ 	for (int i = 0; i < stepsPerRev/4; i++) {
  			digitalWrite(stepPin, HIGH);
  			delayMicroseconds(pulseWidthMicros);
  			digitalWrite(stepPin, LOW);
  			delayMicroseconds(millisBtwnSteps);
+      delayMicroseconds(10000000000);
  	}
  	delay(1000); // One second delay
  	Serial.println(F("Running counter-clockwise"));
  	digitalWrite(dirPin, LOW); //Changes the rotations direction
  	// Makes 400 pulses for making two full cycle rotation
- 	for (int i = 0; i < 2*stepsPerRev; i++) {
+ 	for (int i = 0; i < stepsPerRev/4; i++) {
  			digitalWrite(stepPin, HIGH);
  			delayMicroseconds(pulseWidthMicros);
  			digitalWrite(stepPin, LOW);
  			delayMicroseconds(millisBtwnSteps);
+      delayMicroseconds(10000000000);
  	}
  	delay(1000);
 }
