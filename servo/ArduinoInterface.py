c7528c3d56df3d32c41d4b4ec3d07317fc8464f8
banana <banana99ou@gmail.com>
2023-07-21 01:06:47 +0900

    addtestcode

---
+++ servo/ArduinoInterface.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+
+somePin1 = 26
+somePin2 = 19
+angle = "fine"
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(somePin1, GPIO.OUT)
+
+if angle == "fine":
+    GPIO.output(somePin1, GPIO.HIGH)
+if angle == "coarse":
+    GPIO.output(somePin2, GPIO.HIGH)
---
+++ servo/Rpi_Interface/Rpi_interface.ino
@@ -0,0 +1,24 @@
+int finePin = 10;
+int coarsePin = 11;
+const int stepPin = 3; //Y.STEP
+const int dirPin = 6; // Y.DIR
+const int enPin = 8;
+const float steps/degree = 0.5555555556
+
+void setup() {
+    Serial.begin(9600);
+ 	pinMode(enPin, OUTPUT);
+ 	digitalWrite(enPin, LOW);
+ 	pinMode(stepPin, OUTPUT);
+ 	pinMode(dirPin, OUTPUT);
+    pinMode(finePin, INPUT);
+    pinMode(coarsePin, INPUT);
+
+    // homing routine
+}
+
+void loop() {
+    // read inputPin and step the motor
+    // restrain scan range
+    if(digitalRead(finePin) == HIGH)
+}
