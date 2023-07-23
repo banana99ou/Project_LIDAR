d2dba6080ef736e6bddd3876686653a5b8156195
banana <banana99ou@gmail.com>
2023-07-23 20:48:14 +0900

    moved stepper logic to combined

--- combined/test_1.py
+++ combined/test_1.py
@@ -1,21 +1,19 @@
-#import os
-import pygame
+# import os
 from math import cos, sin, pi, floor
 from adafruit_rplidar import RPLidar
 import RPi.GPIO as GPIO
+import pygame
 import time
 
-stepYPin = 26  # Y.STEP
-dirYPin = 19  # Y.DIR
+stepPin = 26  # Y.STEP
+dirPin = 19  # Y.DIR
 enPin = 13
-stepPin = stepYPin
-dirPin = dirYPin
-homingPin = 4 # placeholderplaceholderplaceholderplaceholderplaceholderplaceholderplaceholderplaceholder
+homingPin = 4  # placeholderplaceholderplaceholderplaceholderplaceholderplaceholderplaceholderplaceholder
 stepsPerRev = 200
 pulseWidthMicros = 100  # microseconds 0.0001 second
 millisBtwnSteps = 1000  # 0.001 second
-degreesPerStep = 200/360
 angleNow = 0
+stepdir = "cw"
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(enPin, GPIO.OUT)
@@ -29,58 +27,58 @@ H = 600
 SCAN_BYTE = b'\x20'
 SCAN_TYPE = 129
 
-def singlestep(dir):
-  if dir == 0: # ccw
-    GPIO.output(dirPin, GPIO.LOW)
-  elif dir == 1: #cw
-    GPIO.output(dirPin, GPIO.HIGH)
-  GPIO.output(stepPin, GPIO.HIGH)
-  time.sleep(pulseWidthMicros / 1000000.0)
-  GPIO.output(stepPin, GPIO.LOW)
-  time.sleep(millisBtwnSteps / 100000.0)
-  
-def StepmotorStep(resolution="fine"):
-    # set resolution
-    if(resolution == "fine"):
-      resolution = 1 # step == 1.8 degree
-    if(resolution == "coarse"):
-       resolution = 3 # == 5.4 degree
-    # need to add a feature that ties scanning range to certain range
-    for i in range(resolution):
-      if angleNow > 45:
-        singlestep(0)
-        angleNow += resolution * degreesPerStep
-def homing():
-  while True:
-    # if homing switch pressed
-    if(GPIO.input(homingPin) == 1):
-      # turn 45 clockwise to move to 90 degree position and initialize lidar pos
-      # then break
-      # find way to use arduino as driver instead
-      angleNow = 45
-      GPIO.output(dirPin, GPIO.HIGH)
-      for i in range(45/1.8):
+def step(dir, angle):
+    # handle stepper motor
+    # takes in dirrection and angle in degrees and keep record of current angle
+    if dir == "cw":
+        GPIO.output(dirPin, GPIO.HIGH)
+        anglestep = 1.8 
+    if dir == "ccw":
+        GPIO.output(dirPin, GPIO.LOW)
+        anglestep = -1.8
+    for i in range(angle/1.8):
         GPIO.output(stepPin, GPIO.HIGH)
         time.sleep(pulseWidthMicros / 1000000.0)
         GPIO.output(stepPin, GPIO.LOW)
         time.sleep(millisBtwnSteps / 100000.0)
-        angleNow += 1.8
-      # angleNow = 90
-      break
-    # if homing switch not pressed
-    else:
-      # keep turning ccw until switch pressed
-      GPIO.output(dirPin, GPIO.LOW)
-      GPIO.output(stepPin, GPIO.HIGH)
-      time.sleep(pulseWidthMicros / 1000000.0)
-      GPIO.output(stepPin, GPIO.LOW)
-      time.sleep(millisBtwnSteps / 100000.0)
+        angleNow += anglestep
+    
+def StepmotorStep(resolution="fine"):
+    # set resolution
+    if resolution == "fine":
+        resolution = 1  # step == 1.8 degree
+    if resolution == "coarse":
+        resolution = 3  # == 5.4 degree
+    if stepdir == "cw" and angleNow <= 135:
+        stepdir = "ccw"
+    if stepdir == "ccw" and angleNow >= 225:
+        stepdir = "cw"
+    # need to add a feature that ties scanning range to certain range
+    step(stepdir, resolution)
+    return angleNow
+
+
+def homing():
+    while True:
+        # if homing switch pressed
+        if (GPIO.input(homingPin) == 1):
+            angleNow = 45 # 25 step
+            # turn 90 clockwise to initialize lidar pos
+            # then break
+            # find way to use arduino as driver instead
+            step("cw", 90)
+            break
+        # if homing switch not pressed
+        else:
+            # keep turning ccw until switch pressed
+            step("ccw", 1.8)
+
 
 # Setup pygame
 pygame.display.init()
-lcd = pygame.display.set_mode((H,H))
+lcd = pygame.display.set_mode((H, H))
 pygame.mouse.set_visible(False)
-lcd.fill((200,0,0))
+lcd.fill((200, 0, 0))
 pygame.display.update()
 
 
@@ -94,24 +92,29 @@ max_distance = 0
 
 def process_data(data):
     global max_distance
-    lcd.fill((0,0,0))
-    point = ( int(W / 2) , int(H / 2) )
-    
-    pygame.draw.circle(lcd,pygame.Color(255, 255, 255),point,10 )
-    pygame.draw.circle(lcd,pygame.Color(100, 100, 100),point,100 , 1 )
-    pygame.draw.line( lcd,pygame.Color(100, 100, 100) , ( 0, int(H/2)),( W , int(H/2) ) )
-    pygame.draw.line( lcd,pygame.Color(100, 100, 100) , ( int(W/2),0),( int(W/2) , H ) )
+    lcd.fill((0, 0, 0))
+    point = (int(W / 2), int(H / 2))
+
+    pygame.draw.circle(lcd, pygame.Color(255, 255, 255), point, 10)
+    pygame.draw.circle(lcd, pygame.Color(100, 100, 100), point, 100, 1)
+    pygame.draw.line(lcd, pygame.Color(100, 100, 100),
+                     (0, int(H/2)), (W, int(H/2)))
+    pygame.draw.line(lcd, pygame.Color(100, 100, 100),
+                     (int(W/2), 0), (int(W/2), H))
 
     for angle in range(360):
         distance = data[angle]
         if distance > 0:                  # ignore initially ungathered data points
-            max_distance = 100 # scale maxdistance value to biggest value among current datamax([min([5000, distance]), max_distance]) check unit of distance val
+            # scale maxdistance value to biggest value among current datamax([min([5000, distance]), max_distance]) check unit of distance val
+            max_distance = 100
             radians = angle * pi / 180.0
             x = distance * cos(radians)
             y = distance * sin(radians)
-            point = ( int(W / 2) + int(x / max_distance * (W/2)), int(H/2) + int(y / max_distance * (H/2) ))
-            pygame.draw.circle(lcd,pygame.Color(255, 0, 0),point,2 )
+            point = (int(W / 2) + int(x / max_distance * (W/2)),
+                     int(H/2) + int(y / max_distance * (H/2)))
+            pygame.draw.circle(lcd, pygame.Color(255, 0, 0), point, 2)
     pygame.display.update()
+
 
 scan_data = [0] * 360
 
@@ -119,7 +122,7 @@ try:
     for scan in lidar.iter_scans():
         for (_, angle, distance) in scan:
             scan_data[min([359, floor(angle)])] = distance
-            StepmotorStep("coarse")
+            StepmotorStep()
         process_data(scan_data)
 
 except KeyboardInterrupt:
@@ -129,7 +132,6 @@ except KeyboardInterrupt:
     lidar.disconnect()
 
 
-      
 # issues:
 # .stop() sends stop byte to lidar so chekc what stop byte should be in doc
 # and discriptor length
@@ -137,7 +139,7 @@ except KeyboardInterrupt:
 # edit cad to have homing switch
 # lidar resolution seems to be bit low check it and find way to increase resolution.
 # it could be done by limiting max distance value from lidar
-# check if mountplate is blocking los to lower angle of lidar 
+# check if mountplate is blocking los to lower angle of lidar
 # move this line to github issue page or readme.md file
 
 # milestone
--- lidar/graphic_lidar.py
+++ lidar/graphic_lidar.py
@@ -1,78 +1,14 @@
 # import os
 from math import cos, sin, pi, floor
 from adafruit_rplidar import RPLidar
-import RPi.GPIO as GPIO
 import pygame
 import time
-
-stepPin = 26  # Y.STEP
-dirPin = 19  # Y.DIR
-enPin = 13
-homingPin = 4  # placeholderplaceholderplaceholderplaceholderplaceholderplaceholderplaceholderplaceholder
-stepsPerRev = 200
-pulseWidthMicros = 100  # microseconds 0.0001 second
-millisBtwnSteps = 1000  # 0.001 second
-angleNow = 0
-stepdir = "cw"
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(enPin, GPIO.OUT)
-GPIO.output(enPin, GPIO.LOW)
-GPIO.setup(stepPin, GPIO.OUT)
-GPIO.setup(dirPin, GPIO.OUT)
 
 W = 600
 H = 600
 
 SCAN_BYTE = b'\x20'
 SCAN_TYPE = 129
-
-def step(dir, angle):
-    # handle stepper motor
-    # takes in dirrection and angle in degrees and keep record of current angle
-    if dir == "cw":
-        GPIO.output(dirPin, GPIO.HIGH)
-        anglestep = 1.8 
-    if dir == "ccw":
-        GPIO.output(dirPin, GPIO.LOW)
-        anglestep = -1.8
-    for i in range(angle/1.8):
-        GPIO.output(stepPin, GPIO.HIGH)
-        time.sleep(pulseWidthMicros / 1000000.0)
-        GPIO.output(stepPin, GPIO.LOW)
-        time.sleep(millisBtwnSteps / 100000.0)
-        angleNow += anglestep
-    
-def StepmotorStep(resolution="fine"):
-    # set resolution
-    if resolution == "fine":
-        resolution = 1  # step == 1.8 degree
-    if resolution == "coarse":
-        resolution = 3  # == 5.4 degree
-    if stepdir == "cw" and angleNow <= 135:
-        stepdir = "ccw"
-    if stepdir == "ccw" and angleNow >= 225:
-        stepdir = "cw"
-    # need to add a feature that ties scanning range to certain range
-    step(stepdir, resolution)
-    return angleNow
-
-
-def homing():
-    while True:
-        # if homing switch pressed
-        if (GPIO.input(homingPin) == 1):
-            angleNow = 45 # 25 step
-            # turn 90 clockwise to initialize lidar pos
-            # then break
-            # find way to use arduino as driver instead
-            step("cw", 90)
-            break
-        # if homing switch not pressed
-        else:
-            # keep turning ccw until switch pressed
-            step("ccw", 1.8)
-
 
 # Setup pygame
 pygame.display.init()
@@ -122,7 +58,6 @@ try:
     for scan in lidar.iter_scans():
         for (_, angle, distance) in scan:
             scan_data[min([359, floor(angle)])] = distance
-            StepmotorStep()
         process_data(scan_data)
 
 except KeyboardInterrupt:
