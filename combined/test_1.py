f6c91faa92a60d92c51c9d46e854ad516d6a8490
banana <banana99ou@gmail.com>
2023-08-03 18:46:06 +0900

    remove visual tepolarirly

--- combined/test_1.py
+++ combined/test_1.py
@@ -2,7 +2,7 @@
 from math import cos, sin, pi, floor
 from adafruit_rplidar import RPLidar,RPLidarException
 import RPi.GPIO as GPIO
-import pygame
+# import pygame
 import time
 import homingtest
 import random
@@ -23,8 +23,8 @@ GPIO.output(enPin, GPIO.LOW)
 GPIO.setup(stepPin, GPIO.OUT)
 GPIO.setup(dirPin, GPIO.OUT)
 
-W = 600
-H = 600
+# W = 600
+# H = 600
 
 SCAN_BYTE = b'\x20'
 SCAN_TYPE = 129
@@ -77,11 +77,11 @@ SCAN_TYPE = 129
 
 
 # Setup pygame
-pygame.display.init()
-lcd = pygame.display.set_mode((H, H))
-pygame.mouse.set_visible(False)
-lcd.fill((200, 0, 0))
-pygame.display.update()
+# pygame.display.init()
+# lcd = pygame.display.set_mode((H, H))
+# pygame.mouse.set_visible(False)
+# lcd.fill((200, 0, 0))
+# pygame.display.update()
 
 # Setup the RPLidar
 PORT_NAME = '/dev/ttyUSB0'
@@ -93,26 +93,27 @@ max_distance = 0
 
 def process_data(data):
     global max_distance
-    lcd.fill((0, 0, 0))
-    point = (int(W / 2), int(H / 2))
+    # lcd.fill((0, 0, 0))
+    # point = (int(W / 2), int(H / 2))
 
-    pygame.draw.circle(lcd, pygame.Color(255, 255, 255), point, 10)
-    pygame.draw.circle(lcd, pygame.Color(100, 100, 100), point, 100, 1)
-    pygame.draw.line(lcd, pygame.Color(100, 100, 100), (0, int(H/2)), (W, int(H/2)))
-    pygame.draw.line(lcd, pygame.Color(100, 100, 100), (int(W/2), 0), (int(W/2), H))
+    # pygame.draw.circle(lcd, pygame.Color(255, 255, 255), point, 10)
+    # pygame.draw.circle(lcd, pygame.Color(100, 100, 100), point, 100, 1)
+    # pygame.draw.line(lcd, pygame.Color(100, 100, 100), (0, int(H/2)), (W, int(H/2)))
+    # pygame.draw.line(lcd, pygame.Color(100, 100, 100), (int(W/2), 0), (int(W/2), H))
 
-    for angle in range(360):
-        distance = data[angle]
-        if distance > 0:                  # ignore initially ungathered data points
-            # scale maxdistance value to biggest value among current datamax([min([5000, distance]), max_distance]) check unit of distance val
-            max_distance = 100
-            radians = angle * pi / 180.0
-            x = distance * cos(radians)
-            y = distance * sin(radians)
-            point = (int(W / 2) + int(x / max_distance * (W/2)),
-                     int(H/2) + int(y / max_distance * (H/2)))
-            pygame.draw.circle(lcd, pygame.Color(255, 0, 0), point, 2)
-    pygame.display.update()
+    # for angle in range(360):
+    #     distance = data[angle]
+    #     if distance > 0:                  # ignore initially ungathered data points
+    #         # scale maxdistance value to biggest value among current datamax([min([5000, distance]), max_distance]) check unit of distance val
+    #         max_distance = 100
+    #         radians = angle * pi / 180.0
+    #         x = distance * cos(radians)
+    #         y = distance * sin(radians)
+    #         point = (int(W / 2) + int(x / max_distance * (W/2)),
+    #                  int(H/2) + int(y / max_distance * (H/2)))
+    #         pygame.draw.circle(lcd, pygame.Color(255, 0, 0), point, 2)
+    # pygame.display.update()
+    print(data)
 
 homingtest.homing()
 
@@ -144,7 +145,7 @@ while True:
     except KeyboardInterrupt:
         print('Stopping.')
         break
-    
+
 lidar.stop()
 lidar.stop_motor()
 lidar.disconnect()
