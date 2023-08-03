f2f310a20a95c796c6d7e01aea4ca84158bc0428
banana <banana99ou@gmail.com>
2023-08-03 17:30:28 +0900

    pretest of test_1.py

---
+++ combined/homingtest.py
@@ -0,0 +1,95 @@
+import RPi.GPIO as GPIO
+import time
+
+# all angle in terms of step
+# 200 step = 360
+# 100 step = 180
+# 50 step = 90
+# 25 step = 45
+# 5 step = 9
+
+class StepperError(Exception):
+    '''error'''
+
+stepPin = 26  # Y.STEP
+dirPin = 19  # Y.DIR
+enPin = 13
+HomingPin = 20
+HomingCommon = 21
+stepsPerRev = 200
+pulseWidthMicros = 100  # microseconds 0.0001 second
+millisBtwnSteps = 1000  # 0.001 second
+angleNow = 0
+stepdir = "cw"
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(enPin, GPIO.OUT)
+GPIO.output(enPin, GPIO.LOW)
+GPIO.setup(stepPin, GPIO.OUT)
+GPIO.setup(dirPin, GPIO.OUT)
+GPIO.setup(HomingPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(HomingCommon, GPIO.OUT)
+GPIO.output(HomingCommon, GPIO.HIGH)
+
+def step(dir, angle):
+    # handle stepper motor
+    # takes in dirrection and angle in degrees and keep record of current angle
+    if dir == "cw":
+        global angleNow
+        GPIO.output(dirPin, GPIO.HIGH)
+        anglestep = 1 
+    if dir == "ccw":
+        GPIO.output(dirPin, GPIO.LOW)
+        anglestep = -1
+    for i in range(int(angle)):
+        GPIO.output(stepPin, GPIO.HIGH)
+        time.sleep(pulseWidthMicros / 1000000.0)
+        GPIO.output(stepPin, GPIO.LOW)
+        time.sleep(millisBtwnSteps / 100000.0)
+        angleNow += anglestep
+        print(angleNow)
+        time.sleep(0.1)
+    
+def StepmotorStep(resolution="fine"):
+    # emergencystop
+    # stop and go back few step
+    if GPIO.input(HomingPin):
+        raise StepperError('Angle Error')
+    # set resolution
+    global stepdir
+    if resolution == "fine":
+        resolution = 1  # == 1,8 degree
+    if resolution == "coarse":
+        resolution = 5  # == 9 degree
+    if stepdir == "cw" and angleNow <= 75:
+        stepdir = "ccw"
+    if stepdir == "ccw" and angleNow >= 125:
+        stepdir = "cw"
+    # need to add a feature that ties scanning range to certain range
+    step(stepdir, resolution)
+
+def homing():
+    while True:
+        # if homing switch pressed
+        if GPIO.input(HomingPin):
+            global angleNow 
+            angleNow = 25 # 45 degree
+            print("reset")
+            print(angleNow)
+            time.sleep(0.5)
+            # turn 90 clockwise to initialize lidar pos
+            # then break
+            # find way to use arduino as driver instead
+            step("cw", 50)
+            time.sleep(1)
+            break
+        # if homing switch not pressed
+        else:
+            # keep turning ccw until switch pressed
+            step("ccw", 1)
+
+homing()
+for i in range(200):
+    StepmotorStep()
+    print("stepping")
+print("end")
--- combined/test_1.py
+++ combined/test_1.py
@@ -1,9 +1,11 @@
 # import os
 from math import cos, sin, pi, floor
-from adafruit_rplidar import RPLidar
+from adafruit_rplidar import RPLidar,RPLidarException
 import RPi.GPIO as GPIO
 import pygame
 import time
+import homingtest
+import random
 
 stepPin = 26  # Y.STEP
 dirPin = 19  # Y.DIR
@@ -27,51 +29,51 @@ H = 600
 SCAN_BYTE = b'\x20'
 SCAN_TYPE = 129
 
-def step(dir, angle):
-    # handle stepper motor
-    # takes in dirrection and angle in degrees and keep record of current angle
-    if dir == "cw":
-        GPIO.output(dirPin, GPIO.HIGH)
-        anglestep = 1.8 
-    if dir == "ccw":
-        GPIO.output(dirPin, GPIO.LOW)
-        anglestep = -1.8
-    for i in range(angle/1.8):
-        GPIO.output(stepPin, GPIO.HIGH)
-        time.sleep(pulseWidthMicros / 1000000.0)
-        GPIO.output(stepPin, GPIO.LOW)
-        time.sleep(millisBtwnSteps / 100000.0)
-        angleNow += anglestep
+# def step(dir, angle):
+#     # handle stepper motor
+#     # takes in dirrection and angle in degrees and keep record of current angle
+#     if dir == "cw":
+#         GPIO.output(dirPin, GPIO.HIGH)
+#         anglestep = 1.8 
+#     if dir == "ccw":
+#         GPIO.output(dirPin, GPIO.LOW)
+#         anglestep = -1.8
+#     for i in range(angle/1.8):
+#         GPIO.output(stepPin, GPIO.HIGH)
+#         time.sleep(pulseWidthMicros / 1000000.0)
+#         GPIO.output(stepPin, GPIO.LOW)
+#         time.sleep(millisBtwnSteps / 100000.0)
+#         angleNow += anglestep
     
-def StepmotorStep(resolution="fine"):
-    # set resolution
-    if resolution == "fine":
-        resolution = 1  # step == 1.8 degree
-    if resolution == "coarse":
-        resolution = 3  # == 5.4 degree
-    if stepdir == "cw" and angleNow <= 135:
-        stepdir = "ccw"
-    if stepdir == "ccw" and angleNow >= 225:
-        stepdir = "cw"
-    # need to add a feature that ties scanning range to certain range
-    step(stepdir, resolution)
-    return angleNow
+# def StepmotorStep(resolution="fine"):
+#     # set resolution
+#     if resolution == "fine":
+#         resolution = 1  # step == 1.8 degree
+#     if resolution == "coarse":
+#         resolution = 3  # == 5.4 degree
+#     if stepdir == "cw" and angleNow <= 135:
+#         stepdir = "ccw"
+#     if stepdir == "ccw" and angleNow >= 225:
+#         stepdir = "cw"
+#     # need to add a feature that ties scanning range to certain range
+#     step(stepdir, resolution)
+#     return angleNow
 
 
-def homing():
-    while True:
-        # if homing switch pressed
-        if (GPIO.input(homingPin) == 1):
-            angleNow = 45 # 25 step
-            # turn 90 clockwise to initialize lidar pos
-            # then break
-            # find way to use arduino as driver instead
-            step("cw", 90)
-            break
-        # if homing switch not pressed
-        else:
-            # keep turning ccw until switch pressed
-            step("ccw", 1.8)
+# def homing():
+#     while True:
+#         # if homing switch pressed
+#         if (GPIO.input(homingPin) == 1):
+#             angleNow = 45 # 25 step
+#             # turn 90 clockwise to initialize lidar pos
+#             # then break
+#             # find way to use arduino as driver instead
+#             step("cw", 90)
+#             break
+#         # if homing switch not pressed
+#         else:
+#             # keep turning ccw until switch pressed
+#             step("ccw", 1.8)
 
 
 # Setup pygame
@@ -80,7 +82,6 @@ lcd = pygame.display.set_mode((H, H))
 pygame.mouse.set_visible(False)
 lcd.fill((200, 0, 0))
 pygame.display.update()
-
 
 # Setup the RPLidar
 PORT_NAME = '/dev/ttyUSB0'
@@ -97,10 +98,8 @@ def process_data(data):
 
     pygame.draw.circle(lcd, pygame.Color(255, 255, 255), point, 10)
     pygame.draw.circle(lcd, pygame.Color(100, 100, 100), point, 100, 1)
-    pygame.draw.line(lcd, pygame.Color(100, 100, 100),
-                     (0, int(H/2)), (W, int(H/2)))
-    pygame.draw.line(lcd, pygame.Color(100, 100, 100),
-                     (int(W/2), 0), (int(W/2), H))
+    pygame.draw.line(lcd, pygame.Color(100, 100, 100), (0, int(H/2)), (W, int(H/2)))
+    pygame.draw.line(lcd, pygame.Color(100, 100, 100), (int(W/2), 0), (int(W/2), H))
 
     for angle in range(360):
         distance = data[angle]
@@ -115,18 +114,35 @@ def process_data(data):
             pygame.draw.circle(lcd, pygame.Color(255, 0, 0), point, 2)
     pygame.display.update()
 
+homingtest.homing()
 
-scan_data = [0] * 360
+scan_data = [0] * 360 
 
-try:
-    for scan in lidar.iter_scans():
-        for (_, angle, distance) in scan:
-            scan_data[min([359, floor(angle)])] = distance
-            StepmotorStep()
-        process_data(scan_data)
+while True:
+    try:
+        for scan in lidar.iter_scans():
+            for (_, angle, distance) in scan:
+                scan_data[min([359, floor(angle)])] = distance
+                homingtest.StepmotorStep()
+            process_data(scan_data)
 
-except KeyboardInterrupt:
-    print('Stopping.')
+    except RPLidarException as e:
+        print(f"RPLidar Exception: {e}")
+        lidar.stop_motor()
+        lidar.disconnect()
+        time.sleep(random.randrange(0.5,1))  # Add a small delay before reconnecting to the sensor
+        lidar.connect()
+        lidar.start_motor()
+
+    except homingtest.StepperError:
+        print("angleNow error")
+        homingtest.step("cw", 20)
+        print("recallibration")
+        homingtest.homing()
+
+    except KeyboardInterrupt:
+        print('Stopping.')
+        break
     lidar.stop()
     lidar.stop_motor()
     lidar.disconnect()
