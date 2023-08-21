dcedc14f674f968072436c08d3a42718597835e1
banana <banana99ou@gmail.com>
2023-08-21 17:29:54 +0900

    rebamp with gpt code

--- servo/Rpi_Interface/Rpi_interface/Rpi_interface.ino
+++ servo/Rpi_Interface/Rpi_interface/Rpi_interface.ino
@@ -1,119 +1,63 @@
-int finePin = 10;
-int coarsePin = 11;
-const int stepPin = 3; //Y.STEP
-const int dirPin = 6; // Y.DIR
-const int enPin = 8;
-const int HomingPin = 20;
-const int HomingCommon = 21;
-const float StepsPerDegree = 0.5555555556;
-const int stepsPerRev = 200;
-int pulseWidthMicros = 100; 	// microseconds
-int millisBtwnSteps = 1000;
-int angleNow = 0;
-String stepdir = "cw";
-float StepToAngle = 360/200;
-bool stringComplete;
-String inputString;
+#include <AccelStepper.h>
 
-void step(String dir, int angle) {
-  // handle stepper motor
-  // takes in direction and angle in degrees and keep record of current angle
-  int anglestep;
-  if (dir == "cw") {
-    digitalWrite(dirPin, HIGH);
-    anglestep = 1;
-    angleNow += anglestep;
-  }
-  if (dir == "ccw") {
-    digitalWrite(dirPin, LOW);
-    anglestep = -1;
-    angleNow += anglestep;
-  }
-  for (int i = 0; i < angle; i++) {
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(pulseWidthMicros);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(millisBtwnSteps);
-  }
-}
+const int stepPin = 3;          // Change this to your actual pin
+const int dirPin = 4;           // Change this to your actual pin
+const int limitSwitchPin1 = 2;  // Change this to your first limit switch pin
+const int limitSwitchPin2 = 5;  // Change this to your second limit switch pin
 
-int StepmotorStep(int resolution=5) {
-  // emergencystop
-  // stop and go back few step
-  int IsScanEdge;
-//  if(digitalRead(HomingPin) == HIGH) {
-//    raise StepperError('Angle Error');
-//  }
-  // set resolution
-//  if (resolution == "fine") {
-//    resolution = 1;  // == 1,8 degree
-//  if (resolution == "coarse") {
-//    resolution = 5;  // == 9 degree
-//  }
-  if (stepdir == "cw" && angleNow >= 125) {  // 225 d
-    // if lidar scan range right edge (from lidar's perspective)
-    Serial.println("CW bound");
-    stepdir = "ccw";
-    IsScanEdge = 1;  // "Right"
-  } else if (stepdir == "ccw" && angleNow <= 75) {  // 135 d
-    // if lidar scan range left edge
-    Serial.println("CCW bound");
-    stepdir = "cw";
-    IsScanEdge = -1;  // "left"
-  } else {
-    IsScanEdge = 0;  // "Nan"
-  }
-  step(stepdir, resolution);
-  return IsScanEdge;
-}
-
-void homing() {
-  while (true) {
-    // if homing switch pressed
-    if(digitalRead(HomingPin) == HIGH) {
-        angleNow = -25;  // 45 degree
-        Serial.println("reset");
-        delay(500);
-        step("cw", 150);
-        delay(1000);
-        break;
-    } else {
-        step("ccw", 1);
-    }
-  }
-}
-
-void serialRead() {
-  // Serial.println("SerialRead");
-  while(Serial.available()){
-    // Serial.println("SerialAvail");
-    char inChar = (char)Serial.read();
-    Serial.print(inChar);
-    inputString += inChar;
-    if(inChar =='\n'){
-      stringComplete = true;
-    }
-    else{
-      stringComplete = false;
-    }
-  }
-}
+AccelStepper stepper(AccelStepper::DRIVER, stepPin, dirPin);
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(enPin, OUTPUT);
-  digitalWrite(enPin, LOW);
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
-  pinMode(finePin, INPUT);
-  pinMode(coarsePin, INPUT);
+  pinMode(limitSwitchPin1, INPUT_PULLUP);
+  pinMode(limitSwitchPin2, INPUT_PULLUP);
+  stepper.setMaxSpeed(2000);  // Adjust this value as needed
+  stepper.setAcceleration(1000);  // Adjust this value as needed
+  stepper.setSpeed(1000);  // Adjust this value as needed
+  
+  homing();
 }
 
 void loop() {
-  serialRead();
-  if(stringComplete){
-    // Echo the received command back to the Raspberry Pi as an acknowledgment
-    Serial.println("ACK: " + inputString);
-    stringComplete = false; // Clear the input buffer
+  if (digitalRead(limitSwitchPin1) == LOW || digitalRead(limitSwitchPin2) == LOW) {
+    Serial.println("LimitSwitchPressed");
+    delay(100);  // Debounce delay
   }
+
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    if (command == 'M') {
+      char direction = Serial.read();
+      int angle = Serial.parseInt();
+      moveStepper(direction, angle);
+    }
+  }
+}
+
+void moveStepper(char direction, int angle) {
+  int steps = angle * (stepsPerRev / 360.0);
+  
+  if (direction == 'C') {
+    stepper.setDirection(1);  // Clockwise
+  } else if (direction == 'A') {
+    stepper.setDirection(-1);  // Anticlockwise
+  }
+  
+  stepper.move(steps);
+  
+  while (stepper.distanceToGo() != 0) {
+    stepper.run();
+    delay(1);
+  }
+  
+  Serial.print("MoveCompleted:");
+  Serial.println(stepper.currentPosition());
+}
+
+void homing() {
+  // Implement your homing logic here
+  // Move the stepper motor to the home position using limit switches
+  // Set the stepper's current position to the home position
+  
+  // Once homing is done, send a completion message to Raspberry Pi
+  Serial.println("HomingCompleted");
 }
