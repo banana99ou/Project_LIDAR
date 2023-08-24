69c32c737d9ace3fd6b78cbec132b4b913edec20
banana <banana99ou@gmail.com>
2023-08-24 21:59:15 +0900

    try only homing

--- combined/test_2.py
+++ combined/test_2.py
@@ -14,16 +14,16 @@ StepModule.init_serial()
 print("homing")
 StepModule.homing()
 StepModule.step(0, 25)
-while True:
-    try:
-        StepModule.step_loop()
+# while True:
+#     try:
+#         StepModule.step_loop()
     
-    except StepModule.StepperError as e:
-        print(f"Stepper Error: {e}")
-        StepModule.step(1, 20)
-        print("recallibrating")
-        StepModule.homing()
+#     except StepModule.StepperError as e:
+#         print(f"Stepper Error: {e}")
+#         StepModule.step(1, 20)
+#         print("recallibrating")
+#         StepModule.homing()
 
-    except KeyboardInterrupt:
-        print('Stopping.')
-        break
+#     except KeyboardInterrupt:
+#         print('Stopping.')
+#         break
