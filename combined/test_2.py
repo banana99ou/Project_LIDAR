7038d60047c1a01ad5115a36bc6778db274f3f93
banana <banana99ou@gmail.com>
2023-09-04 15:12:21 +0900

    idk

--- combined/test_2.py
+++ combined/test_2.py
@@ -12,20 +12,20 @@ serial_thread = threading.Thread(target=read_serial_from_module)
 serial_thread.start()
 
 StepModule.init_serial()
-print("test2: homing")
-StepModule.homing()
+# print("test2: homing")
+# StepModule.homing()
 print("test2: step")
 StepModule.step(0, 25)
-while True:
-    try:
-        StepModule.step_loop()
-
+# while True:
+#     try:
+#         StepModule.step_loop()
+# 
 #     except StepModule.StepperError as e:
 #         print(f"Stepper Error: {e}")
 #         StepModule.step(1, 20)
 #         print("recallibrating")
 #         StepModule.homing()
-
-    except KeyboardInterrupt:
+# 
+    # except KeyboardInterrupt:
         print('Stopping.')
         break
